1504a50859b3ed06e61a4ac8d4b3571bd432f9f7
adityathool <adityathool27@gmail.com>
2026-09-20 15:06:20 +0530

    binary search algorithm

---
+++ code/12_binarySearch.c
@@ -0,0 +1,48 @@
+// #include <stdio.h>
+// int binarySearch(int arr[], int element, int size){
+//     int low = 0;
+//     int high = size-1;
+//     while(low <= high){
+//         int mid = low + (high - low) / 2;
+        
+//         if (arr[mid] == element){
+            
+//             return mid;
+//         }
+//         if (arr[mid]> element){
+//             high = mid-1;
+//         }
+//         else{
+//             low = mid+1;
+//         }
+//     }
+//     return -1;
+
+
+
+// }
+// int main(){
+//     int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//     int size = 10;
+//     int index = binarySearch(arr, 8, size);
+    
+//     // Check if the element was actually found before printing
+//     if (index != -1) {
+//         printf("Element found at index: %d\n", index);
+//     } else {
+//         printf("Element not found\n");
+//     }
+    
+//     return 0;
+// }
+#include <stdio.h>
+
+
+
+
+
+int main(){
+
+    
+    return 0;
+}
